706b9d3727a0bfcdf0c4269b2a67be4d8f7cfab3
Gargikul99 <149108975+Gargikul99@users.noreply.github.com>
2024-01-11 13:01:57 -0600

    Update message.py

--- message.py
+++ message.py
@@ -4,13 +4,13 @@ from twilio.rest import Client
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
 def my_msg():
-    account_sid = 'AC47fba6c272aa3ba38cf148984c92996a'
-    auth_token = '9d8021f72af32c59839f08fc67d44283'
+    account_sid = '<account_sid>'
+    auth_token = '<auth_token>'
     client = Client(account_sid, auth_token)
 
-    from_whatsapp_number='whatsapp:+14155238886'
+    from_whatsapp_number='whatsapp:+<number here>'
     # replace this number with your own WhatsApp Messaging number
-    to_whatsapp_number='whatsapp:+917057622382'
+    to_whatsapp_number='whatsapp:+<number>'
 
 
     client.messages.create(body='intruder....!!!!',
